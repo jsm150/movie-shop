7fe5bfdd01d0762c82a3a32c25ab6493ff5c3f84
Jung Sumin <drt00784@gmail.com>
2026-01-29 00:55:09 +0900

    Init

---
+++ src/main/java/com/movie/shop/api/MovieShopApplication.java
@@ -0,0 +1,13 @@
+package com.movie.shop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/shop/api/MovieShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.shop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
