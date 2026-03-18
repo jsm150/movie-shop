81c8158c6d04f5f8b27167d4fd3448bba90c9848
Jung Sumin <drt00784@gmail.com>
2026-03-18 18:40:10 +0900

    Test: 테스트 컨테이너 DB 버전 변경

--- src/test/java/com/movie/shop/api/configuration/AbstractContainerBase.java
+++ src/test/java/com/movie/shop/api/configuration/AbstractContainerBase.java
@@ -9,7 +9,7 @@ public class AbstractContainerBase {
     static final MySQLContainer MY_SQL_CONTAINER;
 
     static {
-        MY_SQL_CONTAINER = new MySQLContainer("mysql:8.0.36");
+        MY_SQL_CONTAINER = new MySQLContainer("mysql:8.0");
         MY_SQL_CONTAINER.start();
     }
 }
